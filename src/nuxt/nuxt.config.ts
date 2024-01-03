7f30b0ecf880bdcb728352045d3946947ce3ae2d
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-03 22:26:13 +0700

    feat(fe): setup tailwind

--- src/nuxt/nuxt.config.ts
+++ src/nuxt/nuxt.config.ts
@@ -2,5 +2,51 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'RemindMe',
+    }
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_URL || 'http://localhost:8000/api'
+    }
+  },
+
+  tailwindcss: {
+    exposeConfig: true,
+    config: {
+      plugins: [
+        require('flowbite/plugin')
+      ],
+      content: [
+        "./node_modules/flowbite/**/*.{js,ts}"
+      ],
+      theme: {
+        extend: {
+          colors: {
+            nabitu: {
+              '50': '#f1fcf4',
+              '100': '#defae9',
+              '200': '#bff3d2',
+              '300': '#8ce9b0',
+              '400': '#53d585',
+              '500': '#2cbb64',
+              '600': '#1e9b4f',
+              '700': '#1b7a41',
+              '800': '#195a33',
+              '900': '#184f2f',
+              '950': '#072c17',
+            },
+          }
+        }
+      }
+    },
+  }
 })
