3a57de054a3c2882b0fe8b884e92060a71dbe5ee
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-06 18:41:30 +0700

    feat(reminder): create model, migration, factory, user relation

---
+++ src/database/factories/ReminderFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reminder>
+ */
+class ReminderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $eventAt = fake()->dateTimeBetween('+1 day', '+1 year');
+
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->title(),
+            'description' => fake()->text(),
+            'remind_at' => fake()->dateTimeBetween('+1 hour', $eventAt)->getTimestamp(),
+            'event_at' => $eventAt->getTimestamp(),
+        ];
+    }
+}
