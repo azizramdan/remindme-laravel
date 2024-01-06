4b60ac09b0994fa355981749cfa5f990865bc2ad
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-06 14:19:23 +0700

    feat(fe): auto refresh access token 5 seconds before it expires

--- src/nuxt/plugins/01.init.ts
+++ src/nuxt/plugins/01.init.ts
@@ -5,10 +5,14 @@ export default defineNuxtPlugin(async () => {
   const accessToken = useCookie(accessTokenCookieKey)
   const refreshToken = useCookie(refreshTokenCookieKey)
   const user = useCookie(userCookieKey)
+  const config = useRuntimeConfig()
 
   if (accessToken.value && refreshToken.value && user.value) {
     store.setAccessToken(accessToken.value)
     store.setRefreshToken(refreshToken.value)
     store.setUser(user.value as unknown as User)
+
+    // Refresh access token 5 seconds before it expires
+    setInterval(() => store.refreshAccessToken(), 1000 * (config.public.accessTokenExpiration - 5))
   }
 })
