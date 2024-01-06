11947984da842d1b9dba89c4781ba7243cfc7e9e
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-06 15:29:29 +0700

    fix: navbar item backgroud color

--- src/nuxt/layouts/default.vue
+++ src/nuxt/layouts/default.vue
@@ -63,10 +63,10 @@ const logout = () => {
         <div :class="{'md:hidden': !useAuthStore().isAuthenticated}" class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <NuxtLink to="/reminders" active-class="md:bg-nabitu-100 dark:md:bg-nabitu-900" class="block py-2 px-3 rounded md:bg-transparent md:text-nabitu-700 md:p-2 hover:bg-nabitu-100 dark:hover:bg-nabitu-900 dark:text-white md:dark:text-nabitu-500" aria-current="page">Reminder</NuxtLink>
+              <NuxtLink to="/reminders" active-class="bg-nabitu-100 dark:bg-nabitu-900" class="block py-2 px-3 rounded md:text-nabitu-700 md:p-2 hover:bg-nabitu-100 dark:hover:bg-nabitu-900 dark:text-white md:dark:text-nabitu-500" aria-current="page">Reminder</NuxtLink>
             </li>
             <li>
-              <button @click="logout" active-class="md:bg-nabitu-100 dark:md:bg-nabitu-900" class="w-full text-left py-2 px-3 rounded md:bg-transparent md:text-nabitu-700 md:p-2 hover:bg-nabitu-100 dark:hover:bg-nabitu-900 dark:text-white md:dark:text-nabitu-500">Logout</button>
+              <button @click="logout" active-class="bg-nabitu-100 dark:bg-nabitu-900" class="w-full text-left py-2 px-3 rounded md:text-nabitu-700 md:p-2 hover:bg-nabitu-100 dark:hover:bg-nabitu-900 dark:text-white md:dark:text-nabitu-500">Logout</button>
             </li>
           </ul>
         </div>
