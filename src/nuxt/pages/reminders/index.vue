3c9a6a6ecf78c57981ef6ddf9d222481fc5ddad7
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-08 00:29:41 +0700

    refactor(fe): move unixToDateTime function to utils

--- src/nuxt/pages/reminders/index.vue
+++ src/nuxt/pages/reminders/index.vue
@@ -48,21 +48,6 @@ async function getData() {
   }
 }
 
-function unixToDateTime(unix: number) {
-  const date = new Date(unix * 1000);
-
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-  };
-
-  return new Intl.DateTimeFormat('en-US', options).format(date);
-}
-
 function showDeleteModal(reminder: TReminder) {
   useEventEmit('modal:reminder:delete', reminder)
 }
---
+++ src/nuxt/utils/index.ts
@@ -0,0 +1,14 @@
+export function unixToDateTime(unix: number) {
+  const date = new Date(unix * 1000);
+
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
