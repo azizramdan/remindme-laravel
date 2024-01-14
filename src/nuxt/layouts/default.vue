d66cfe96c9afe63b111e08dca13a40865c3a7a4a
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-14 12:41:20 +0700

    feat(fe): add navbar-right-menu

--- src/nuxt/layouts/default.vue
+++ src/nuxt/layouts/default.vue
@@ -8,6 +8,7 @@ useHead({
 })
 
 const isDarkMode = ref(false)
+const user = computed(() => useAuthStore().user)
 
 watch(isDarkMode, () => {
   if (isDarkMode.value) {
@@ -46,26 +47,33 @@ const logout = () => {
           <img src="https://arlab.dev/logo.png" class="h-8 invert dark:invert-0" alt="Logo" />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">RemindMe</span>
         </NuxtLink>
-        <button @click="isDarkMode = !isDarkMode" id="theme-toggle" type="button" title="Toggle dark mode" class="md:order-last text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-          <svg v-if="isDarkMode" id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-          </svg>
-          <svg v-else id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-          </svg>
-        </button>
-        <button :class="{'hidden': !useAuthStore().isAuthenticated}" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <div class="flex gap-3 md:order-last">
+          <button @click="isDarkMode = !isDarkMode" id="theme-toggle" type="button" title="Toggle dark mode" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+            <svg v-if="isDarkMode" id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
+            </svg>
+            <svg v-else id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+            </svg>
+          </button>
+
+          <button v-if="user" type="button" data-dropdown-toggle="navbar-right-menu" class="hidden md:block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+            <img class="inline-block h-6 w-6 rounded-sm ring-2 ring-white" :src="`https://ui-avatars.com/api/?name=${user.name}&font-size=0.6&bold=true`" alt="">
+          </button>
+        </div>
+
+        <button :class="{ 'hidden': !useAuthStore().isAuthenticated }" data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
           </svg>
         </button>
-        <div :class="{'md:hidden': !useAuthStore().isAuthenticated}" class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div :class="{ 'md:hidden': !useAuthStore().isAuthenticated }" class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-slate-900 dark:border-gray-700">
             <li>
               <NuxtLink to="/reminders" active-class="bg-nabitu-100 dark:bg-nabitu-900" class="block py-2 px-3 rounded md:text-nabitu-700 md:p-2 hover:bg-nabitu-100 dark:hover:bg-nabitu-900 dark:text-white md:dark:text-nabitu-500" aria-current="page">Reminder</NuxtLink>
             </li>
-            <li>
+            <li class="md:hidden">
               <button @click="logout" active-class="bg-nabitu-100 dark:bg-nabitu-900" class="w-full text-left py-2 px-3 rounded md:text-nabitu-700 md:p-2 hover:bg-nabitu-100 dark:hover:bg-nabitu-900 dark:text-white md:dark:text-nabitu-500">Logout</button>
             </li>
           </ul>
@@ -78,5 +86,20 @@ const logout = () => {
     </div>
 
     <TheToastContainer />
+
+    <div v-if="user" class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow min-w-48 dark:bg-gray-700" id="navbar-right-menu">
+      <ul class="py-2 font-medium" role="none">
+        <li>
+          <span class="block px-4 py-2 text-sm text-gray-700 font-bold hover:bg-gray-100 dark:text-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
+            {{ user?.name }}
+          </span>
+        </li>
+        <li>
+          <button @click="logout" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
+            Logout
+          </button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>  
