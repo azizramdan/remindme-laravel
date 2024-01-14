3b9a5744ab9699c94b2fc8b50b66945e0847f3d9
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-14 09:43:59 +0700

    feat(reminder-fe): validate using vee-validate on edit form

--- src/nuxt/pages/reminders/[id]/edit.vue
+++ src/nuxt/pages/reminders/[id]/edit.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-const form = reactive({
-  id: 0,
-  title: '',
-  event_at: '',
-  remind_at: '',
-  description: '',
+import { reminderSchema } from '~/schemas/reminder';
+import type { TReminder } from '~/types/reminder';
+
+const reminder = ref<TReminder>()
+
+const initialValues = computed(() => {
+  if (!reminder.value) return {}
+
+  return {
+    title: reminder.value.title,
+    event_at: unixTimestampToDateTime(reminder.value.event_at),
+    remind_at: unixTimestampToDateTime(reminder.value.remind_at),
+    description: reminder.value.description,
+  }
 })
 
 const isBusy = reactive({
@@ -23,13 +31,7 @@ async function getData() {
       throw error
     }
 
-    const reminder = data.value.data
-
-    form.id = reminder.id
-    form.title = reminder.title
-    form.event_at = unixTimestampToDateTime(reminder.event_at)
-    form.remind_at = unixTimestampToDateTime(reminder.remind_at)
-    form.description = reminder.description
+    reminder.value = data.value.data
   } catch (error: any) {
     if (error.value?.statusCode === 404) {
       throw showError({
@@ -54,37 +56,14 @@ function unixTimestampToDateTime(unixTimestamp: number): string {
   return `${year}-${month}-${day}T${hours}:${minutes}`;
 }
 
-async function update() {
-  const eventAt = dateStringToUnix(form.event_at)
-  const remindAt = dateStringToUnix(form.remind_at)
-  const now = dateStringToUnix((new Date()).toLocaleString())
-
-  if (eventAt < now) {
-    alert('Event date cannot be in the past')
-    return
-  }
-
-  if (remindAt < now) {
-    alert('Reminder date cannot be in the past')
-    return
-  }
-
-  if (eventAt < remindAt) {
-    alert('Event date cannot be before reminder date')
-    return
-  }
-
+async function update(values: any) {
+  const form = reminderSchema.cast(values)
 
   try {
     isBusy.update = true
 
-    const { data, error } = await useApiPut(`/reminders/${form.id}`, {
-      body: {
-        title: form.title,
-        event_at: eventAt,
-        remind_at: remindAt,
-        description: form.description
-      },
+    const { data, error } = await useApiPut(`/reminders/${reminder.value!.id}`, {
+      body: form,
       pick: ['ok', 'data'],
     })
 
@@ -106,24 +85,28 @@ async function update() {
 </script>
 
 <template>
-  <div class="bg-white p-8 rounded-lg mx-2 dark:bg-gray-800">
-    <form class="space-y-4 md:space-y-6" @submit.prevent="update">
-      <div>
-        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event Name</label>
-        <input type="text" name="title" id="title" v-model="form.title" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-nabitu-600 focus:border-nabitu-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Meeting with Bob" required>
-      </div>
-      <div>
+  <div class="bg-white p-8 rounded-lg mx-2 dark:bg-gray-800" v-if="initialValues.title">
+    <Form class="space-y-4 md:space-y-6" @submit="update" :validation-schema="reminderSchema" :initial-values="initialValues">
+      <Field as="div" name="title" v-slot="{ field }">
+        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+        <input v-bind="field" type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-nabitu-600 focus:border-nabitu-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Meeting with Bob">
+        <ErrorMessage class="mt-2 text-sm text-red-600 dark:text-red-500" name="title" />
+      </Field>
+      <Field as="div" name="event_at" v-slot="{ field }">
         <label for="event_at" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event Date & Time</label>
-        <input type="datetime-local" name="event_at" id="event_at" v-model="form.event_at" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-nabitu-600 focus:border-nabitu-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-      </div>
-      <div>
+        <input v-bind="field" type="datetime-local" name="event_at" id="event_at" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-nabitu-600 focus:border-nabitu-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <ErrorMessage class="mt-2 text-sm text-red-600 dark:text-red-500" name="event_at" />
+      </Field>
+      <Field as="div" name="remind_at" v-slot="{ field }">
         <label for="remind_at" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Reminder Date & Time</label>
-        <input type="datetime-local" name="remind_at" id="remind_at" v-model="form.remind_at" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-nabitu-600 focus:border-nabitu-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-      </div>
-      <div>
+        <input v-bind="field" type="datetime-local" name="remind_at" id="remind_at" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-nabitu-600 focus:border-nabitu-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <ErrorMessage class="mt-2 text-sm text-red-600 dark:text-red-500" name="remind_at" />
+      </Field>
+      <Field as="div" name="description" v-slot="{ field }">
         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-        <textarea id="description" v-model="form.description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-nabitu-600 focus:border-nabitu-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discuss about new project related to new system" required></textarea>
-      </div>
+        <textarea v-bind="field" id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-nabitu-600 focus:border-nabitu-600 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Discuss about new project related to new system"></textarea>
+        <ErrorMessage class="mt-2 text-sm text-red-600 dark:text-red-500" name="description" />
+      </Field>
 
       <div class="flex justify-center mt-32">
         <button type="submit" :disabled="isBusy.update" class="flex gap-1 w-full text-white bg-nabitu-800 hover:bg-nabitu-700 focus:ring-4 focus:outline-none focus:ring-nabitu-500 font-medium rounded-lg text-sm px-5 py-2.5 justify-center disabled:bg-nabitu-900 disabled:hover:bg-nabitu-900 dark:bg-nabitu-800 dark:hover:bg-nabitu-700 dark:focus:ring-nabitu-800">
