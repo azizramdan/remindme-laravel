6707c6986b45167d698a6527d8162fecb3643f58
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-14 10:56:35 +0700

    fix: format date time in reminder mail

--- src/resources/views/mail/reminder.blade.php
+++ src/resources/views/mail/reminder.blade.php
@@ -1,4 +1,5 @@
 @inject('carbon', 'Carbon\Carbon')
+@inject('htmlString', 'Illuminate\Support\HtmlString')
 
 <x-mail::layout>
 <x-slot:header>
@@ -11,9 +12,15 @@
 
 This is a friendly reminder about the upcoming event in your schedule.
 
-**Title**: {{ $reminder->title }}  
-**Date and Time**: {{ $carbon::parse($reminder->event_at)->format('F j, Y, g:i a') }}  
-**Description**: {{ $reminder->description }}  
+**Title**:  
+{{ $reminder->title }}
+
+**Date and Time**:  
+{{ $carbon::parse($reminder->event_at)->format('F j, Y, g:i a T') }}  
+{{ $carbon::parse($reminder->event_at)->setTimezone('+0700')->format('F j, Y, g:i a T') }}  
+
+**Description**:  
+{{ new $htmlString(nl2br($reminder->description)) }}  
 
 <x-slot:footer>
 <tr>
