9c134854e69da1052b29bb94f171fc8e5df0302d
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-01-14 10:41:34 +0700

    fix(fe): action button color in reminder detail page

--- src/nuxt/pages/reminders/[id]/index.vue
+++ src/nuxt/pages/reminders/[id]/index.vue
@@ -42,7 +42,7 @@ function showDeleteModal() {
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
         </svg>
       </NuxtLink>
-      <NuxtLink :to="`/reminders/${reminder.id}/edit`" title="Edit" class="hover:text-nabitu-600 p-2 dark:hover:text-white">
+      <NuxtLink :to="`/reminders/${reminder.id}/edit`" title="Edit" class="hover:text-yellow-400 p-2 dark:hover:text-white">
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="m13.835 7.578-.005.007-7.137 7.137 2.139 2.138 7.143-7.142-2.14-2.14Zm-10.696 3.59 2.139 2.14 7.138-7.137.007-.005-2.141-2.141-7.143 7.143Zm1.433 4.261L2 12.852.051 18.684a1 1 0 0 0 1.265 1.264L7.147 18l-2.575-2.571Zm14.249-14.25a4.03 4.03 0 0 0-5.693 0L11.7 2.611 17.389 8.3l1.432-1.432a4.029 4.029 0 0 0 0-5.689Z" />
         </svg>
